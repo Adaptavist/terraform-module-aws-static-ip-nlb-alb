493524c6e03421907419f49d1902547a93fe2a2b
dominikdrzymala <ddrzymala@adapatvist.com>
2025-06-25 09:57:44 +0100

    fix: New version s3.

--- modules/nlb_target_group_configurer/storage.tf
+++ modules/nlb_target_group_configurer/storage.tf
@@ -1,6 +1,6 @@
 module "aws-s3-encrypted-private" {
   source  = "Adaptavist/aws-s3-encrypted-private/module"
-  version = "1.5.1"
+  version = "3.1.5"
 
   bucket_suffix                         = "${var.name}-nlb-config-${var.tags["Avst:Stage:Name"]}"
   use_bucket_suffix_as_name             = true
